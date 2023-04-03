a1b4e9b4b3bd6fdb107a6fcee749d2a479b86b73
gemtunde <gemtunde@gmail.com>
2023-04-03 18:06:33 +0100

    user and product schema

--- server/server.js
+++ server/server.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+//models
+const User = require("./models/user")
+
+//loads env files
+dotenv.config();
 
 const app = express();
+
+//db conection
+// mongoose.connect(
+//     "mongodb+srv://gemtunde:JJ5j5hH8NCzHq2fc@amazon.kuvx1hb.mongodb.net/?retryWrites=true&w=majority",
+// {useNewUrlParser: true, useUnifiedTopology : true},
+// err=> {
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("connected to db")
+//     }
+// });
+
+mongoose
+     .connect( process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
+     .then(() => console.log( 'Database Connected' ))
+     .catch(err => console.log( err ));
 
 //midddleware
 app.use(morgan('dev'));
@@ -17,7 +42,18 @@ app.get('/', (req,res) => {
 
 //post data from frontend to server
 app.post("/", (req, res)=> {
-    console.log(req.body.name)
+   let user = new User();
+   user.name = req.body.name ;
+   user.email = req.body.email ;
+   user.password = req.body.password ;
+
+//    user.save(err=>{
+//     err ? res.json(err) : res.json("user successfully saved");
+//    })
+   user.save()
+   .then(()=>res.json("user saved successful"))
+   .catch(err=>console.log(err))
+
 })
 
 
